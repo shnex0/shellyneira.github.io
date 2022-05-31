29c8614ee3b3864a422c2a99341e9860c6e10cce
sneirabastos@miu.edu <shellynbastos@gmail.com>
2022-05-30 19:51:35 -0500

    Add javascript in assignments 04.0 and 04.1

---
+++ assignment-04.0/js/script.js
@@ -0,0 +1,5 @@
+function submit(){
+    console.log("Email:", document.getElementById("inputEmail").value);
+    console.log("Password:", document.getElementById("inputPassword").value);
+    console.log("Website:", document.getElementById("inputWebsite").value);
+}
---
+++ assignment-04.1/js/script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form')
+
+form.onsubmit = (e) => {
+  e.preventDefault()
+  console.log('submitted')
+
+  const productNumber = document.getElementById('inputProductNumber').value;
+  const stockQuantity = document.getElementById('inputStockQuantity').value;
+  const name = document.getElementById('inputName').value;
+  const supplier = document.getElementById('inputSupplier').value;
+  const productName = document.getElementById('inputProductName').value;
+  const dateSupplied = document.getElementById('inputDateSupplied').value;
+   
+  const modal = new bootstrap.Modal(document.getElementById('productModal'), {keyboard: false})
+   
+  document.getElementById('productModalLabel').innerHTML = 'Product &quot;' + name + '&quot; added successfully'
+  
+  document.getElementById('productBody').innerHTML = 
+      '<h5>Product Information:</h5>'+
+      'Product Number: ' + productNumber +
+      '<br>Stock Quantity: ' + stockQuantity +
+      '<br>Name: ' + name +
+      '<br>Supplier: ' + supplier +
+      '<br>Product Name: ' + productName +
+      '<br>Date Supplied: ' + dateSupplied;
+
+  modal.show();
+}
